0641a81bd37857ef28e8e2345212b2fc68cba84b
Cody Dracars <cdracars@gmail.com>
2013-01-14 12:33:39 -0600

    initial commit

---
+++ recipes/default.rb
@@ -0,0 +1,28 @@
+#
+# Cookbook Name:: mysql-secure
+# Recipe:: default
+#
+# Copyright 2012, Dracars Designs
+#
+# All rights reserved - Do Not Redistribute
+#
+
+require_recipe "mysql::server"
+
+execute "remove test database" do
+  command "mysql -u root -p#{node['mysql']['server_root_password']} -e \"DROP DATABASE test;\""
+  only_if { File.exists?(node['mysql']['data_dir'] + '/test') }
+end
+
+execute "stop root access from all but localhost" do
+  command "mysql -u root -p#{node['mysql']['server_root_password']} -e \"DELETE FROM mysql.user WHERE User='root' AND Host!='localhost';\""
+end
+
+execute "remove blank users" do
+  command "mysql -u root -p#{node['mysql']['server_root_password']} -e \"DELETE FROM mysql.user WHERE User='';\""
+end
+
+
+execute "flush privileges" do
+  command "mysql -u root -p#{node['mysql']['server_root_password']} -e \"Flush PRIVILEGES;\""
+end
